7b2360f912aee1882936848da9ec374f0e9a3d75
pondkarun <pondkarun@gmail.com>
2021-07-27 02:14:44 +0700

    Preloader

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import Head from "next/head"
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
@@ -7,26 +7,25 @@ import "moment/locale/th";
 import locale from 'antd/lib/locale/th_TH';
 import { useRouter } from 'next/dist/client/router';
 import { Cookies } from 'react-cookie'
+import Preloader from './Preloader'
+import "moment/locale/th";
 
 function Layout({ children }) {
     const route = useRouter()
-    const isComponentMounted = useRef(true)
+    const [loader, setLoader] = useState(true);
 
     useEffect(() => {
-        if (isComponentMounted.current) {
-            (() => {
-                const cookies = new Cookies();
-                const token = cookies.get('token');
-                if (!token) route.push("/signin")
-            })();
-        }
-
-        return () => {
-            isComponentMounted.current = false
-        }
+        const cookies = new Cookies();
+        const token = cookies.get('token');
+        if (!token) route.push("/signin")
     })
 
-    return (
+    useEffect(() => {
+        setTimeout(() => setLoader(false), 1000);
+    }, [])
+
+    return loader ? <Preloader /> : (
+
         <ConfigProvider locale={locale}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
